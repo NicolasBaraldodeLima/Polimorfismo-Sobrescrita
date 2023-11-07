4186bed17c40e074e61aab6038da809faae2cf45
Nicolas Baraldo <121053856+NicolasBaraldodeLima@users.noreply.github.com>
2023-11-07 19:49:14 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+
+    public void emitirSom() {
+        System.out.println("Este é um som genérico de um animal");
+    }
+}
---
+++ Cachorro.java
@@ -0,0 +1,11 @@
+
+
+    class Cachorro extends Animal {
+        @Override
+        public void emitirSom() {
+            System.out.println("O cachorro faz 'Au, Au");
+        }
+    }
+
+
+
